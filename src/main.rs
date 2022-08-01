bef45da7a8226e87767f97ff3e5f35bb47b0338f
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2022-08-01 18:45:08 -0300

    refactor: Move epidemic printing to function

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,10 @@ mod path;
 mod person;
 mod rand;
 
-fn main() {
-  let size = 1000;
-  let time = 2;
-
-  let mut epidemic = epidemic::Epidemic::new(
-    0.3,
-    1,
-    10,
-    size,
-    0.1,
-    0.01,
-  );
-
-  println!(
-    "epidemic(0): {:?}",
-    epidemic
-      .persons
-      .iter()
-      .map(|p| (if p.infected { "i" } else { "h" }, p.time))
-      .collect::<Vec<_>>()
-  );
-
-  for _ in 0..time {
-    epidemic.time_step();
-  }
-
+fn print_epidemic(
+  time: usize,
+  epidemic: &epidemic::Epidemic,
+) {
   println!(
     "epidemic({time}): {:?}",
     epidemic
@@ -38,4 +16,19 @@ fn main() {
       .map(|p| (if p.infected { "i" } else { "h" }, p.time))
       .collect::<Vec<_>>()
   );
+}
+
+fn main() {
+  let time = 2;
+
+  let mut epidemic =
+    epidemic::Epidemic::new(0.3, 1, 10, 1000, 0.1, 0.01);
+
+  print_epidemic(0, &epidemic);
+
+  for _ in 0..time {
+    epidemic.time_step();
+  }
+
+  print_epidemic(time, &epidemic);
 }
