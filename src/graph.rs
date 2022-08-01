a14b3c7b31b9e689e5f81b8c6db9994f77fa446d
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2022-08-01 20:30:37 -0300

    refactor: Remove method density from graph::Graph

--- src/graph.rs
+++ src/graph.rs
@@ -19,20 +19,6 @@ impl Graph {
     self.data[row * self.size + col] = value
   }
 
-  pub fn density(&self) -> f32 {
-    let mut edges: usize = 0;
-
-    for row in 0..self.size {
-      for col in 0..self.size {
-        if self.get(row, col) {
-          edges += 1;
-        }
-      }
-    }
-
-    edges as f32 / (self.size * (self.size - 1)) as f32
-  }
-
   fn max_data_density(&self) -> f32 {
     (self.size as f32 - 1.0) / self.size as f32
   }
